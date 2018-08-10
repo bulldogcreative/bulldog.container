ecdb0a7b4f475d7dfb426c8d92f6811ec7c6d0f4
Levi Durfee <levi.durfee@gmail.com>
2018-08-10 10:43:02 -0400

    Add tests for get

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -4,14 +4,35 @@ use PHPUnit\Framework\TestCase;
 
 class ContainerTest extends TestCase
 {
+    private $container;
+
+    public function setUp()
+    {
+        $this->container = new Bulldog\Container\Container;
+    }
+
     public function testSet()
     {
         $id = 'id';
         $value = 'bulldog';
 
-        $container = new Bulldog\Container\Container;
-
-        $result = $container->set($id, $value);
+        $result = $this->container->set($id, $value);
         $this->assertSame($result, $value);
+    }
+
+    /**
+     * @expectedException Bulldog\Container\NotFoundException
+     */
+    public function testGetException()
+    {
+        $result = $this->container->get('not-in-there');
+    }
+
+    /**
+     * @expectedException Psr\Container\NotFoundExceptionInterface
+     */
+    public function testGetExceptionInterface()
+    {
+        $result = $this->container->get('not-in-there');
     }
 }

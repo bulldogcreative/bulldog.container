ff1a2bccb91e5f59d6e15648974dbe3925b51624
Levi Durfee <levi.durfee@gmail.com>
2018-08-10 10:40:33 -0400

    Write code for `get` method

--- src/Container.php
+++ src/Container.php
@@ -10,12 +10,20 @@ class Container implements ContainerInterface
 
     public function get($id)
     {
+        if($this->has($id)) {
+            return $this->container[$id];
+        }
 
+        throw new \Bulldog\Container\NotFoundException();
     }
 
     public function has($id)
     {
-     //
+        if(isset($this->container[$id])) {
+            return true;
+        }
+
+        return false;
     }
 
     public function set($id, $value)

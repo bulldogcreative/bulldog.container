cfebb7b39242f809949a1edd68da5e6baeef89a6
Levi Durfee <levi.durfee@gmail.com>
2018-08-29 08:30:01 -0400

    Add API Docs (#2)
    * Ignore sami.phar
    
    * Add simple config for Sami
    
    * Ignore cache file
    
    * Set build dir and cache dir
    
    * Add docs

---
+++ sami.php
@@ -0,0 +1,15 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in('./src')
+;
+
+return new Sami($iterator, [
+    'build_dir'            => './docs',
+    'cache_dir'            => './.cache',
+]);

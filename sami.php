1a4e78622a4d400adeaa2c01ed720fa62bd7f31e
Levi Durfee <levi.durfee@gmail.com>
2018-08-29 08:24:43 -0400

    Add simple config for Sami

---
+++ sami.php
@@ -0,0 +1,12 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in('./src')
+;
+
+return new Sami($iterator);
